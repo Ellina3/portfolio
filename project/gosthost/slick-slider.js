9dcc331c01ad76078bcd9ad98c5062186cf7073b
ellina <novira@tut.by>
2018-12-09 01:11:30 +0300

    finish files

---
+++ project/gosthost/slick-slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $('.fade').slick({
+        dots: true,
+        infinite: true,
+        speed: 400,
+        fade: true,
+        cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    });
+
+});
